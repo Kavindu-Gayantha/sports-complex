f5c361c040a8b0ce290a1520c11424046a13dfc4
kavindu gayantha <kavinduariyarathna44@gmail.com>
2019-05-07 07:44:45 +0530

    m
    k

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE loggedin
+
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(50) NOT NULL
+)
---
+++ loggedin.Table_1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [user name] NCHAR(10) NULL, 
+    [password] NCHAR(10) NULL
+)
